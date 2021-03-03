2f86c7ea8caaf2f674fc5751d31496982c443c16
RomitSingh1999 <62353481+RomitSingh1999@users.noreply.github.com>
2021-03-03 20:33:50 +0530

    Update settings.py

--- anime/settings.py
+++ anime/settings.py
@@ -130,5 +130,3 @@ STATICFILES_DIRS=[
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 django_heroku.settings(locals())
-MEDIA_URL = '/media/'
-MEDIA_ROOT= os.path.join(BASE_DIR,'media')
